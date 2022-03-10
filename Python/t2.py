b5f4c6d77cae26fc7e5bfe4ac5606bd88b1c2082
tovijendra <vij.lingappa@yahoo.co.in>
2022-03-10 18:18:07 +0530

    second commit2

---
+++ Python/t2.py
@@ -0,0 +1,3 @@
+Git: Git is a distributed version control system for tracking changes in source code during software development. It is designed for coordinating work among programmers, but it can be used to track changes in any set of files. Its goals include speed, data integrity, and support for distributed, non-linear workflows. 
+
+GitHub: GitHub is a web-based Git repository hosting service, which offers all of the distributed revision control and source code management (SCM) functionality of Git as well as adding its own features. 
